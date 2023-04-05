e3b3d51a6ecd9158fb08729a89ab22b05e758909
3257525958 <ma4301511@gmail.com>
2023-04-05 05:44:11 +0430

    change8

--- clinic1402/settings.py
+++ clinic1402/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+# import locale
+#
+# locale.setlocale(locale.LC_ALL,"fa_IR.UTF-8")
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
