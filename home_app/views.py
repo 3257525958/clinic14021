140f6f85072b166beaced0056658054c3b98efa4
3257525958 <ma4301511@gmail.com>
2023-05-24 08:41:15 +0330

    add_user12.1

--- home_app/views.py
+++ home_app/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render
 from cantact_app.models import accuntmodel
 
 # Create your views here.
-
+profilestatus =['']
 def home(request):
-     # profilestatus = 'کاربر معمولی'
     if request.user.is_authenticated:
         us = accuntmodel.objects.all()
         for u in us:
             if u.melicode == request.user.username:
-                profilestatus = f"{u.level} {u.firstname} {u.lastname}"
+                profilestatus[0] = f"{u.level} {u.firstname} {u.lastname}"
     else:
-        profilestatus = 'ورود به کاربری'
+        profilestatus[0] = 'ورود به کاربری'
 
     return render(request,'home.html',context={
-                                                'profilestatus':profilestatus,
+                                                'profilestatus':profilestatus[0],
     })
