d9deb975cf682f2e089b2a06d55ee33477900093
3257525958 <ma4301511@gmail.com>
2023-06-25 23:46:02 +0330

    job

--- clinic1402/settings.py
+++ clinic1402/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home_app.apps.HomeAppConfig',
     'cantact_app.apps.CantactAppConfig',
-     'jalali_date',
+    'jalali_date',
+    'jobs_app.apps.JobsAppConfig',
 
 
 ]
