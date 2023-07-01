d6bb00fe5017ceef2bee51887495ac64fd1a13c4
3257525958 <ma4301511@gmail.com>
2023-07-02 00:11:03 +0330

    job

--- jobs_app/admin.py
+++ jobs_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from jobs_app.models import jobsmodel , employeemodel , servicmodel
+from jobs_app.models import jobsmodel , employeemodel
+# from jobs_app.models import  servicmodel
 
 admin.site.register(jobsmodel)
 admin.site.register(employeemodel)
-admin.site.register(servicmodel)
+# admin.site.register(servicmodel)
--- jobs_app/models.py
+++ jobs_app/models.py
@@ -11,11 +11,11 @@ class employeemodel(models.Model):
     melicod = models.CharField(max_length=10,default='0')
     def __str__(self):
         return f"{self.melicod}"
-class servicmodel(models.Model):
-    servic = models.CharField(max_length=150 , default='')
-    emp = models.CharField(max_length=150 , default='')
-    time = models.CharField(max_length=150 , default='')
-    cast = models.CharField(max_length=150 , default='')
-
-    def __str__(self):
-        return f"{self.servic}"
+# class servicmodel(models.Model):
+#     servic = models.CharField(max_length=150 , default='')
+#     emp = models.CharField(max_length=150 , default='')
+#     time = models.CharField(max_length=150 , default='')
+#     cast = models.CharField(max_length=150 , default='')
+#
+#     def __str__(self):
+#         return f"{self.servic}"
