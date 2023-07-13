44cd395d983e093a41ebbfa432481973c1b02271
3257525958 <ma4301511@gmail.com>
2023-07-13 09:07:00 +0330

    reserv

--- reserv_app/views.py
+++ reserv_app/views.py
@@ -4,9 +4,15 @@ ww = ['t']
 def reservdef(request):
     works = workmodel.objects.all()
     ww.clear()
+    ww.append('start')
     for w in works :
-        ww.append(w)
-
+        a = 0
+        for x in ww :
+            if x ==  w.work :
+                a = 1
+        if a == 0 :
+            ww.append(w.work)
+    ww.pop(0)
     return render(request,'reserv.html',context={'works':works,
-
-    })
+                                                 'job':ww,
+                                                 })
