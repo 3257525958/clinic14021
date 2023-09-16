9107212359e2e29544bb12f0102b6fb9d1da85a2
3257525958 <ma4301511@gmail.com>
2023-09-16 14:40:51 +0330

    ترمیم تقویم

--- static/js/calandarr.js
+++ static/js/calandarr.js
@@ -3,11 +3,6 @@
     a = v /1000;
     console.log(a);
     if (a >= 1 ){
-        // document.getElementsByClassName('textobject')[0].innerHTML = 'تقویم';
-        // document.getElementsByClassName('calanderhead')[0].hidden = false;
-        // document.getElementsByClassName('calandeer')[0].hidden = false;
-        f = document.getElementById("face");
-        f.click();
         Swal.fire({
           position: 'top-end',
           icon: 'success',
@@ -16,5 +11,10 @@
           timer: 2000
                  });
 
+        // document.getElementsByClassName('textobject')[0].innerHTML = 'تقویم';
+        // document.getElementsByClassName('calanderhead')[0].hidden = false;
+        // document.getElementsByClassName('calandeer')[0].hidden = false;
+        f = document.getElementById("face");
+        f.click();
     }
  }
